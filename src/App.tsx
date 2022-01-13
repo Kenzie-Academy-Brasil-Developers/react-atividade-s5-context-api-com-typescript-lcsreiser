022c7c8836bf01688f00898bfbd9de4417a82644
Lucas <lcssreiser@gmail.com>
2022-01-13 18:27:47 -0300

    feat, project done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Product from "./components/Products/index";
+
+function App() {
+  return (
+    <div className="App">
+      <Product />
+    </div>
+  );
+}
+
+export default App;
